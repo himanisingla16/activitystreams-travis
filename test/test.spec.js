16688ddca8a78ef3284ef445305f33a72d2a4456
Himani <himani.singla@niit.com>
2017-07-28 17:06:17 +0530

    test cases should run

--- test/test.spec.js
+++ test/test.spec.js
@@ -2,15 +2,19 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
+let expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Circle API', function() {
 	it('should save circle without error', (done) => {
 		chai.request('http://localhost:4000')
 		.post('/circle')
+		.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsiY2lyY2xlczphbGwiLCJmb2xsb3dzOmFsbCIsIm1haWxib3g6YWxsIl0sImlhdCI6MTUwMDU3MDYyMX0.YqHdtxTPeq5UoT9yUhQw9gziURvdHAfaiALOwlhGCTg`)
 		.end((err, res) => {
 			res.should.have.status(200);
-			done();
-		});
+			expect(res.body).to.be.an('object').to.have.property('circleId');
+			expect(res.body).to.be.an('object').to.have.property('mailboxId');
+			expect(res.body).to.be.an('object').to.have.property('createdOn');
+			done();});
 	});
 });
