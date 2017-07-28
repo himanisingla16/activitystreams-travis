eb7162462d477b8ffe07168838e531cd71c2db38
Himani <himani.singla@niit.com>
2017-07-28 16:53:32 +0530

    added test cases

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,5 +1,16 @@
-const _ = require('underscore');
 
-_.each([1,2], function(a,i){
-	console.log(a);
-})
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let should = chai.should();
+chai.use(chaiHttp);
+
+describe('Circle API', function() {
+	it('should save circle without error', (done) => {
+		chai.request('http://localhost:4000')
+		.post('/circle')
+		.end((err, res) => {
+			res.should.have.status(200);
+			done();
+		});
+	});
+});
