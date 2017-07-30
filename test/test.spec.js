523df18f36519dc9e019432d5522a02353a4b028
Himani <himani.singla@niit.com>
2017-07-30 14:40:59 +0530

    sd

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,12 +1,12 @@
-/*let chai = require('chai');
+let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
 let expect = chai.expect;
-chai.use(chaiHttp);*/
+chai.use(chaiHttp);
 
 describe('Circle API', function() {
 	it('should save circle without error', (done) => {
-		/*chai.request('http://localhost:4000')
+		chai.request('http://localhost:4000')
 		.post('/circle')
 		.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsiY2lyY2xlczphbGwiLCJmb2xsb3dzOmFsbCIsIm1haWxib3g6YWxsIl0sImlhdCI6MTUwMDU3MDYyMX0.YqHdtxTPeq5UoT9yUhQw9gziURvdHAfaiALOwlhGCTg`)
 		.end((err, res) => {
@@ -14,6 +14,6 @@ describe('Circle API', function() {
 			expect(res.body).to.be.an('object').to.have.property('circleId');
 			expect(res.body).to.be.an('object').to.have.property('mailboxId');
 			expect(res.body).to.be.an('object').to.have.property('createdOn');
-			done();});*/
+			done();});
 	});
 });
