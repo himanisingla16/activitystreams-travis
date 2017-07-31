459cee5776def0c3469b62d426f36be5f74bacbf
Himani <himani.singla@niit.com>
2017-07-31 11:08:56 +0530

    ded

--- test/test.spec.js
+++ test/test.spec.js
@@ -5,7 +5,7 @@ let expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Circle API', function() {
-	it('should save circle without error', (done) => {
+	it('circles mailbox gets all the messages immediately', (done) => {
 		chai.request('http://localhost:4000')
 		.post('/circle')
 		.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsiY2lyY2xlczphbGwiLCJmb2xsb3dzOmFsbCIsIm1haWxib3g6YWxsIl0sImlhdCI6MTUwMDU3MDYyMX0.YqHdtxTPeq5UoT9yUhQw9gziURvdHAfaiALOwlhGCTg`)
@@ -16,6 +16,27 @@ describe('Circle API', function() {
 			expect(res.body).to.be.an('object').to.have.property('circleId');
 			expect(res.body).to.be.an('object').to.have.property('mailboxId');
 			expect(res.body).to.be.an('object').to.have.property('createdOn');
-			done();});
+			let circleId = res.body.circleId;
+			let circleMailboxId = res.body.mailboxId;
+			if(circleId && circleMailboxId){
+				chai.request('http://localhost:4000')
+				.post(`/circle/${circleMailboxId}/activity`)
+				.set('Authorization', `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsiY2lyY2xlczphbGwiLCJmb2xsb3dzOmFsbCIsIm1haWxib3g6YWxsIl0sImlhdCI6MTUwMDU3MDYyMX0.YqHdtxTPeq5UoT9yUhQw9gziURvdHAfaiALOwlhGCTg`)
+				.send({ link: 'www.facebook.com' })
+        		.expect('Content-Type', /json/)
+        		.end((err, res) => {
+        			console.log(res);
+					console.log(err);
+          			if (err) { done(err); return; }
+          			res.should.have.status(201);
+            		expect(res.body).to.be.an('object').to.have.property('payload');
+            		expect(resbody.payload).to.be.an.('object').to.have.property('link');
+            		done();
+        		});
+			}
+			else{
+				done();
+			}
+		});
 	});
 });
